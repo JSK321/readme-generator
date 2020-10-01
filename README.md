048a02a3445fd553f55d4faef31f5b872281d271
Jae Kim <jaeshinkim321@gmail.com>
2020-10-01 16:39:50 -0700

    readme.md is generated

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# readme-generator
+
+        # README
+
+        ## Description
+            creates read me
+        
+        ## Table of Contents
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [License](#license)
+        - [Test](#test)
+        - [Contributors](#contributors)
+        - [Questions](#questions)
+        
+        ## Installation
+            npm install inquire
+
+        ## Usage
+            generates readme easily
+        
+        ## License
+            MIT
+
+        ## Test
+            none
+
+        ## Contributors
+            me
+
+        ## Questions
+            jsk321
+            jae Kim
+        
---
+++ generateMD.js
@@ -0,0 +1,37 @@
+// function generateMD(data){
+//     return `
+//         #${data.tilte}
+
+//         ## Description
+//             ${data.description}
+        
+//         ## Table of Contents
+//         -[Installation](#installation)
+//         -[Usage](#usage)
+//         -[License](#license)
+//         -[Test](#test)
+//         -[Contributors](#contributors)
+//         -[Questions](#questions)
+        
+//         ## Installation
+//             ${data.install}
+
+//         ## Usage
+//             ${data.usage}
+        
+//         ## License
+//             ${data.license}
+
+//         ## Test
+//             ${data.test}
+
+//         ## Contributors
+//             ${data.contributor}
+
+//         ## Questions
+//             ${data.github}
+//             ${data.email}`
+    
+// }
+
+// module.exports = generateMD();
